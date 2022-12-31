53dda1eba8dc12df4d1bebc975f087ca3cfbb146
shubhams83 <shubhams@techis.io>
2022-12-31 16:12:43 +0530

    notes added

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# moviezoneapp-reactjs-project1
-This Project covers the fundamentals &amp;amp; terminologies of React JS which includes Node, NPM, NPX, Component, JSX, Hooks, State, Conditionals, Virtual DOM, Babel, etc.
+# The Movie Zone App - React JS - Project - 1
+
+This Project covers the fundamentals &amp; terminologies of React JS which includes Node, NPM, NPX, Component, JSX, Hooks, State, Conditionals, Virtual DOM, Babel, etc.
+
+[LIVE HOSTED WEBSITE REPLIT](https://moviezoneapp-reactjs-project1.shubhamshriva15.repl.co/)
+
+# Screenshot
+
+![Screenshot 2022-12-26 at 4 49 03 PM](https://user-images.githubusercontent.com/115470266/209543201-866a26f9-0cce-4185-a60c-e0f368b2bd9a.png)
+
+# Technologies Used
+
+* React JS Library
+* HTML - 5
+* CSS - 3
+* JS ES6
+* GitHub Version Control System
+* Replit Deployment
+
+# Usage
+
+* Download the Project Repository Zip File.
+* Run the React Server => npm start
+* Termiante the Server => Ctrl+C
+
+React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies.
+
---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import searchIcon from './search.svg'
+import MovieCard from './MovieCard'
+
+const API_URL = 'https://www.omdbapi.com?apikey=ab06f71e'
+
+// const movie1 = {
+//     "Title": "Peaky Blinders",
+//     "Year": "2022",
+//     "imdbID": "tt2442560",
+//     "Type": "series",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BZjYzZDgzMmYtYjY5Zi00YTk1LThhMDYtNjFlNzM4MTZhYzgyXkEyXkFqcGdeQXVyMTE5NDQ1MzQ3._V1_SX300.jpg"
+// }
+
+const App = () => {
+
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+        setMovies(data.Search)
+        // console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovies('')
+    }, [])
+
+    return (
+        <>
+            <div className="app">
+                <h1>MovieZone</h1>
+
+                <div className="search">
+                    <input type="text"
+                        placeholder='Search for movies'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <img
+                        src={searchIcon}
+                        alt="search"
+                        onClick={() => searchMovies(searchTerm)}
+                    />
+                </div>
+
+                {movies?.length > 0 ?
+                    (
+                        <>
+                            <div className="container">
+                                {movies.map((movie) => {
+                                    return (
+                                        <MovieCard key={movie.id} movie={movie} />
+                                    )
+                                })}
+                            </div>
+                        </>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )}
+            </div>
+        </>
+
+    )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component } from 'react';
+
+// class App extends Component {
+//     render() {
+//         return(
+//             <h1>Hello, React!</h1>
+//         )
+//     }
+// }
