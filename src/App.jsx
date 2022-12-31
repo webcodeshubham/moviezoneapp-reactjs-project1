dd24251decc46ca5546749dbe5250852c9a836f4
shubhams83 <shubhams@techis.io>
2022-12-31 17:37:42 +0530

    final debugg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import searchIcon from './search.svg'
-import MovieCard from './MovieCard'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import searchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-const API_URL = 'https://www.omdbapi.com?apikey=ab06f71e'
+const API_URL = "https://www.omdbapi.com?apikey=ab06f71e";
 
 // const movie1 = {
 //     "Title": "Peaky Blinders",
@@ -14,81 +14,60 @@ const API_URL = 'https://www.omdbapi.com?apikey=ab06f71e'
 // }
 
 const App = () => {
+  // store the current state of the application and perform any necessary operations.
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [movies, setMovies] = useState([])
-    const [searchTerm, setSearchTerm] = useState('')
+  // Asynchronous Function to retrieve the list of movies from the database
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    // console.log(data.Search)
+  };
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`)
-        const data = await response.json()
-        setMovies(data.Search)
-        // console.log(data.Search)
-    }
+  useEffect(() => {
+    searchMovies("");
+  }, []);
 
-    useEffect(() => {
-        searchMovies('')
-    }, [])
+  return (
+    <>
+      <div className="app">
+        <h1>MovieZone</h1>
 
-    return (
-        <>
-            <div className="app">
-                <h1>MovieZone</h1>
-
-                <div className="search">
-                    <input type="text"
-                        placeholder='Search for movies'
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <img
-                        src={searchIcon}
-                        alt="search"
-                        onClick={() => searchMovies(searchTerm)}
-                    />
-                </div>
-
-                {movies?.length > 0 ?
-                    (
-                        <>
-                            <div className="container">
-                                {movies.map((movie) => {
-                                    return (
-                                        <MovieCard key={movie.id} movie={movie} />
-                                    )
-                                })}
-                            </div>
-                        </>
-                    ) : (
-                        <div className="empty">
-                            <h2>No movies found</h2>
-                        </div>
-                    )}
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search for movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+            src={searchIcon}
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
+        {/* // Optional Chaining Mechanism */}
+        {movies?.length > 0 ? (
+          <>
+            <div className="container">
+              {movies.map((movie) => {
+                return <MovieCard key={movie.id} movie={movie} />;
+              })}
             </div>
-        </>
+          </>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-    )
-}
-
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
 
 // import { Component } from 'react';
 
